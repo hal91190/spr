4dcdee4fb5986100b0a89fc525c9343c5e4db34c
Stéphane Lopes <stephane.lopes@uvsq.fr>
2025-03-07 11:23:22 +0100

    Refactor l'évaluation

--- spr/evaluation.py
+++ spr/evaluation.py
@@ -9,10 +9,14 @@ import datetime
 from spr.config import CONFIG
 from spr.student import Student
 from spr.grade import Grade
-from spr.cistats import collect_commits_stats_from_repository
+from spr.cistats import collect_commits_stats_from_repository, CommitsStats
+
+NO_NUMBER = "NO_NUMBER"
+NO_LASTNAME = "NO_LASTNAME"
+NO_FIRSTNAME = "NO_FIRSTNAME"
 
 
-@dataclass(frozen=True)
+@dataclass(init=False)
 class Evaluation:
     """Result of the evaluation of a repository for a student."""
 
@@ -55,6 +59,24 @@ class Evaluation:
     evaluation: int
     "Result of the evaluation"
 
+    def __init__(
+        self, student: Student, grade: Grade, ci_stats: CommitsStats, result: int
+    ):
+        """Create an evaluation from a student, a grade, stats about commits and a result."""
+        self.number = student.number
+        self.lastname = student.lastname
+        self.firstname = student.firstname
+        self.github_username = grade.github_username
+        self.repository_name = grade.repository_name
+        self.repository_url = grade.repository_url
+        self.nb_commits = ci_stats.nb_commits
+        self.first_commit_datetime = ci_stats.first_commit_datetime
+        self.last_commit_datetime = ci_stats.last_commit_datetime
+        self.min_time_between_commits = ci_stats.min_time_between_commits
+        self.avg_time_between_commits = ci_stats.avg_time_between_commits
+        self.avg_msg_length = ci_stats.avg_msg_length
+        self.evaluation = result
+
     def __getitem__(self, index: int) -> Any:
         """Access to attributes by index.
 
@@ -70,58 +92,54 @@ class Evaluation:
 def evaluate_repositories(
     students: list[Student], grades: list[Grade]
 ) -> list[Evaluation]:
-    """Run a list of command in students repositories and collect results."""
+    """Run a list of commands in students repositories and collect results."""
     logger = logging.getLogger(__name__)
     evaluations = []
     for grade in grades:
         student = find_student_with_grade(grade, students)
-        # evaluate repository
-        repository_path = grade.repository_name
-        if os.path.isdir(repository_path):
-            ci_stats = collect_commits_stats_from_repository(repository_path)
-            current_working_directory = os.getcwd()
-            os.chdir(repository_path)
-            environment = os.environ.copy() | CONFIG.environment
-            result = 0
-            for command in CONFIG.commands:
-                completed_process = subprocess.run(
-                    command,
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL,
-                    env=environment,
-                )
-                result += 1 if completed_process.returncode == 0 else 0
-                logger.debug(
-                    "Running '%s' from '%s' (%d)",
-                    command,
-                    repository_path,
-                    completed_process.returncode,
-                )
-            logger.info("Result for %s = %d", student, result)
-            evaluations.append(
-                Evaluation(
-                    student.number if student else "NO_NUMBER",
-                    student.lastname if student else "NO_LASTNAME",
-                    student.firstname if student else "NO_FIRSTNAME",
-                    grade.github_username,
-                    grade.repository_name,
-                    grade.repository_url,
-                    ci_stats.nb_commits,
-                    ci_stats.first_commit_datetime,
-                    ci_stats.last_commit_datetime,
-                    ci_stats.min_time_between_commits,
-                    ci_stats.avg_time_between_commits,
-                    ci_stats.avg_msg_length,
-                    result,
-                )
-            )
-            os.chdir(current_working_directory)
+        if os.path.isdir(grade.repository_name):
+            logger.info("Evaluating %s for %s", grade.repository_name, student)
+            ci_stats = collect_commits_stats_from_repository(grade.repository_name)
+            result = evaluate_repository(student, grade.repository_name)
+            evaluations.append(Evaluation(student, grade, ci_stats, result))
         else:
-            logger.fatal("No directory named %s", repository_path)
+            logger.fatal("No directory named %s", grade.repository_name)
     return evaluations
 
 
-def find_student_with_grade(grade: Grade, students: list[Student]) -> Student | None:
+def evaluate_repository(student: Student, repository_path: str) -> int:
+    """Run a list of commands in a repository and return the number of successful commands."""
+    logger = logging.getLogger(__name__)
+    current_working_directory = os.getcwd()
+    os.chdir(repository_path)
+    environment = os.environ.copy() | CONFIG.environment
+    result = 0
+    for command in CONFIG.commands:
+        result += execute_command(command, environment)
+    logger.info("Result for %s = %d", student, result)
+    os.chdir(current_working_directory)
+    return result
+
+
+def execute_command(command: list[str], environment: dict[str, str]) -> int:
+    """Run a command and return 1 if the command was successful, 0 otherwise."""
+    logger = logging.getLogger(__name__)
+    completed_process = subprocess.run(
+        command,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        env=environment,
+    )
+    logger.debug(
+        "Running '%s' (%d)",
+        command,
+        completed_process.returncode,
+    )
+
+    return 1 if completed_process.returncode == 0 else 0
+
+
+def find_student_with_grade(grade: Grade, students: list[Student]) -> Student:
     """Find a student in the list of students from the identifier in the grade."""
     logger = logging.getLogger(__name__)
     # match grade with student list
@@ -129,7 +147,7 @@ def find_student_with_grade(grade: Grade, students: list[Student]) -> Student | 
     matching_students = list(
         filter(lambda s: s.number == student_from_grade.number, students)
     )
-    student = None
+    student = Student(NO_NUMBER, NO_LASTNAME, NO_FIRSTNAME)
     if len(matching_students) == 0:
         logger.warning("No matching student for %s", student_from_grade)
     elif len(matching_students) > 1:  # many matching students
